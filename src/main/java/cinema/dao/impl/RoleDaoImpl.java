cb266ec10bf885e4f7841fe44d3d6d01a42bff6c
Nikita <katkovnikitka@gmail.com>
2023-07-12 11:40:03 +0200

    v 1.0

---
+++ src/main/java/cinema/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,31 @@
+package cinema.dao.impl;
+
+import cinema.dao.AbstractDao;
+import cinema.dao.RoleDao;
+import cinema.exception.DataProcessingException;
+import cinema.model.Role;
+import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    @Autowired
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Optional<Role> getByName(String roleName) {
+        try (Session session = factory.openSession()) {
+            return session.createQuery("select r from Role r "
+                            + "where r.roleName = :roleName", Role.class)
+                    .setParameter("roleName", Role.RoleName.valueOf(roleName))
+                    .uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException("Couldn't get role by name: " + roleName, e);
+        }
+    }
+}
